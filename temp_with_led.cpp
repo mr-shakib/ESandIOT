2c7d993df2a35538765a29092f7ac6b7fc6fb20b
Shakib <132547096+mr-shakib@users.noreply.github.com>
2025-07-14 01:06:49 +0600

    Update temp_with_led.cpp

--- temp_with_led.cpp
+++ temp_with_led.cpp
@@ -1,13 +1,16 @@
-// C++ code
-//
-#define led 13
+#define yellowLed 11
+#define greenLed 12
+#define redLed 13
+
 const int tmp36 = A0;
 
 
 void setup(){
   Serial.begin(9600);
   analogReference(DEFAULT);
-  pinMode(led,OUTPUT);
+  pinMode(yellowLed,OUTPUT);
+  pinMode(greenLed,OUTPUT);
+  pinMode(redLed,OUTPUT);
   
   
 }
@@ -24,14 +27,21 @@ void loop(){
   Serial.print("°C");
   Serial.println();
   
-  if(tempC >=30){
-    digitalWrite(led, HIGH);
-    delay(1000);
-    digitalWrite(led, LOW);
-    delay(1000);
+  if(tempC <= 25){
+    digitalWrite(greenLed, HIGH);
+    digitalWrite(yellowLed, LOW);
+    digitalWrite(redLed, LOW);
+    
+  }else if( tempC > 25 && tempC <=40){
+    digitalWrite(greenLed, LOW);
+    digitalWrite(yellowLed, HIGH);
+    digitalWrite(redLed, LOW);
+    
   }else{
-    digitalWrite(led, LOW);
-  }
+    digitalWrite(greenLed, LOW);
+    digitalWrite(yellowLed, LOW);
+    digitalWrite(redLed, HIGH);
+	}
   
   delay(1000);
 }
